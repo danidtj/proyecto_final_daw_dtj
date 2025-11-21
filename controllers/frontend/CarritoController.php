958939e3d75d64509bd18698e59ce4b099544b31
Daniel T. <danidtj@gmail.com>
2025-11-21 17:32:35 +0100

    Eliminación de archivos que finalmente no forman parte del proyecto

--- controllers/frontend/CarritoController.php
+++ controllers/frontend/CarritoController.php
@@ -4,62 +4,6 @@ namespace ControllerFrontend;
 
 @session_start();
 
-
-
- // Clave secreta (reemplazar con la clave real)
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*if (isset($_SESSION['id_usuario'])) {
-        //echo "Sesión iniciada como: " . $_SESSION['usuario']['nombre_usuario'];
-
-        require_once dirname(__DIR__) . '/utilidades/SessionStorageController.php';
-        //$_SESSION['productosAlmacenados'];
-
-        if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['eleccionCarta']) && !empty($_POST['productosCarrito'])) {
-
-            $storage = new SessionStorageController();
-            $productos = array();
-
-            $carritoController = new CarritoController();
-
-            if (!empty($storage->getSessionStorage("productosCarrito"))) {
-                //$productos[] = $storage->getSessionStorage("productosCarrito");
-                //$storage->removeSessionStorage("productosCarrito");
-                $storage->setSessionStorage("productosCarrito", $_POST['productosCarrito']);
-                var_dump($storage->getSessionStorage("productosCarrito"));
-                array_push($_SESSION['productosAlmacenados'], $storage->getSessionStorage("productosCarrito"));
-            } else {
-                $storage->setSessionStorage("productosCarrito", $_POST['productosCarrito']);
-                $storage->setSessionStorage("productosAlmacenados", $storage->getSessionStorage("productosCarrito"));
-                //$_SESSION['productosAlmacenados'] = $storage->getSessionStorage("productosCarrito");
-            }
-        }
-
-        $carritoController = new CarritoController();
-        $carritoController->mostrarVistaCarrito();
-    } else {
-        echo '<script>
-        alert("Debes iniciar sesión para acceder a esta página.");
-        window.location.href = "../../home";
-    </script>';
-
-        // En caso de que JavaScript esté deshabilitado
-        header("Refresh: 3; URL=../../home"); // Espera 3 segundos y redirige
-        exit();
-    }
-}*/
-
 class CarritoController
 {
     public function mostrarVistaCarrito()
@@ -70,60 +14,6 @@ class CarritoController
     
 }
 
-/*require_once dirname(__DIR__, 2) . '/views/frontend/cesta.php';
 
-// Comprobamos si se quiere añadir un producto a la cesta
-if (isset($_POST['comanda'])) {
-    $cesta->newArticulo($_POST['cod']);
-    $cesta->saveCesta();
-}
-
-class CarritoController {
-    protected $productos = array();
-
-    // Introduce un nuevo artículo en la cesta de la compra
-    public function newArticulo($codigo) {
-        $producto = Carrito::getProducto($codigo);
-        $this->productos[] = $producto;
-    }
-    
-    // Obtiene los artículos en la cesta
-    public function getProductos() { 
-        return $this->productos; 
-    }
-    
-    // Obtiene el coste total de los artículos en la cesta
-    /*public function getCoste() {
-        $coste = 0;
-        foreach ($this->productos as $p) {
-            $coste += $p->getPVP();
-        }
-        return $coste;
-    }*/
-    
-    // Devuelve true si la cesta está vacía
-    /*public function isVacia() {
-        return (count($this->productos) == 0);
-    }
-    
-    // Guarda la cesta de la compra en la sesión del usuario
-    public function saveCesta() { 
-        $_SESSION['cesta'] = $this; 
-    }
-    
-    // Recupera la cesta de la compra almacenada en la sesión del usuario
-    public static function loadCesta() {
-        if (!isset($_SESSION['cesta'])) {
-            return new CarritoController();
-        } else {
-            return $_SESSION['cesta'];
-        }
-    }
-}
-
-?>*/
-
-
-/* --- SCRIPT JS PARA GESTIONAR ELIMINAR SIN RECARGAR Y ACTUALIZAR PRECIO --- */
 ?>
 
--- views/frontend/reserva.php
+++ views/frontend/reserva.php
@@ -78,7 +78,7 @@ $orden = new Orden();
                                 <option value="15:50">15:50</option>
                                 <option value="16:00">16:00</option>
                                 <option value="16:30">16:30</option>
-                                <option value="17:16">17:16</option>
+                                <option value="17:00">17:00</option>
                             </optgroup>
                             <optgroup label="Noche">
                                 <option value="20:00">20:00</option>
